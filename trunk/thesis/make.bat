a98a92576cfd87a120ef488686a6745e920dc63e
bestrada <bestrada@810dc73e-211a-0410-9d22-83b59d889060>
2008-10-17 02:55:39 +0000

    Share project "thesis" into "https://bestrada.googlecode.com/svn"

---
+++ trunk/thesis/make.bat
@@ -0,0 +1,23 @@
+del *.pdf
+
+del *.aux
+del *.bbl
+del *.blg
+del *.toc
+del *.log
+del *.lof
+
+pdflatex thesis.tex
+bibtex thesis
+pdflatex thesis.tex
+pdflatex thesis.tex
+
+del *.aux
+del *.bbl
+del *.blg
+del *.toc
+del *.log
+del *.lof
+
+pause
+cls
