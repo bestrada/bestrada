3882bdaab4dbe70b052b355354eaba42daadb496
bestrada <bestrada@810dc73e-211a-0410-9d22-83b59d889060>
2009-06-24 22:34:11 +0000

    finished the tutorial!

--- trunk/python_stuff/mysite/urls.py
+++ trunk/python_stuff/mysite/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^polls/', include('mysite.polls.urls')),
     # Example:
     # (r'^mysite/', include('mysite.foo.urls')),
 
@@ -13,5 +14,5 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
+    (r'^admin/(.*)', admin.site.root),
 )
