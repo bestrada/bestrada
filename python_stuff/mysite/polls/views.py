eee786ae750e0d50b42059d1f349791c2d298af2
bestrada <bestrada@810dc73e-211a-0410-9d22-83b59d889060>
2009-06-24 22:34:11 +0000

    finished the tutorial!

---
+++ python_stuff/mysite/polls/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404, render_to_response
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from mysite.polls.models import Choice, Poll
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
